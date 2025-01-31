c08e559ba7f21e7b384ae9ed741171aff547310b
mowlan4 <87225853+mowlan4@users.noreply.github.com>
2025-01-31 16:36:33 +0000

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,22 +10,21 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
           
 Passionate about science and <span className="purple">innovation</span>,
- I am a driven student who excels in chemistry, physics, and mathematics. <br></br>
+ I am a driven student who excels in logical thinking and problem solving. <br></br>
  I participate in STEM competitions,
   run an online web design business, 
   and actively engage in scientific research and lectures. <br>
     </br>
   My projects include building gadgets like a 
   <span className="purple"> homemade spectrometer</span> and <span className="purple">portable water filtration system. </span>
-  With aspirations to explore space by building my own rocket,
-   I am committed to pushing the boundaries of knowledge and technology.
-          </p>
+  
+I have also made hydra.fun ( a chatapp which has over 100 members )         </p>
           
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "With so much conflict in the world, space exploration can be a beacon of hope."{" "}
+            "If you put your mind to it, anything is possible."{" "}
           </p>
-          <footer className="blockquote-footer">Anne McClain</footer>
+          <footer className="blockquote-footer">Kimberly Guilfoylen</footer>
         </blockquote>
       </Card.Body>
     </Card>
